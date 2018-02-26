f7e67ad4b8acc327e28be08db635b19f24837dae
Ryan Zec <code@ryanzec.com>
2018-02-25 20:02:58 -0500

    minor tweaks to build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-var path = require('path');
-var process = require('process');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var CleanObsoleteChunks = require('webpack-clean-obsolete-chunks');
-var LiveReloadPlugin = require('webpack-livereload-plugin');
-var CircularDependencyPlugin = require('circular-dependency-plugin');
-var extractSass = new ExtractTextPlugin({
+let path = require('path');
+let process = require('process');
+let webpack = require('webpack');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let CleanObsoleteChunks = require('webpack-clean-obsolete-chunks');
+let LiveReloadPlugin = require('webpack-livereload-plugin');
+let CircularDependencyPlugin = require('circular-dependency-plugin');
+let extractSass = new ExtractTextPlugin({
   filename: 'main-[chunkhash].css',
-  allChunks: true
+  allChunks: true,
 });
 // var babelPlugins = [
 //   'transform-runtime',
@@ -18,30 +18,29 @@ var extractSass = new ExtractTextPlugin({
 const isDevMode = process.env.NODE_ENV === 'development';
 
 // if (!isDevMode) {
-  // babelPlugins.push('transform-react-constant-elements');
-  // babelPlugins.push('transform-react-inline-elements');
-  //NOTE:current way to pass through props make this optomization not possible right now(need to add to .babelrctoo)
-  //babelPlugins.push('transform-react-remove-prop-types');
+// babelPlugins.push('transform-react-constant-elements');
+// babelPlugins.push('transform-react-inline-elements');
+//NOTE:current way to pass through props make this optomization not possible right now(need to add to .babelrctoo)
+//babelPlugins.push('transform-react-remove-prop-types');
 // }
 
-var webpackConfig = {
+let webpackConfig = {
   resolve: {
     extensions: ['.js', '.jsx'],
     // modules: [__dirname, 'node_modules']
     alias: {
       src: path.resolve(__dirname, 'src'),
-      app: path.resolve(__dirname, 'web/app')
-    }
+      app: path.resolve(__dirname, 'web/app'),
+    },
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: [
-        path.resolve(__dirname, 'node_modules')
-      ],
-      use: {
-        loader: 'babel-loader',
-        /*query: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        use: {
+          loader: 'babel-loader',
+          /*query: {
           presets: [
             [
               'env', {
@@ -54,98 +53,100 @@ var webpackConfig = {
           ],
           plugins: babelPlugins
         }*/
-      }
-    }, {
-      test: /\.module\.(css|scss)$/,
-      use: extractSass.extract({
-        fallback: 'style-loader',
-        use: [{
-          loader: 'css-loader',
-          options: {
-            importLoaders: 3,
-            modules: true,
-            sourceMap: isDevMode,
-            localIdentName: isDevMode ? '[name]__[local]___[hash:base64:5]' : '[hash:base64]',
-          }
-        }, {
-          loader: 'postcss-loader',
-          options: {
-            sourceMap: isDevMode
-          }
-        }, {
-          loader: 'resolve-url-loader',
-          query: {
-            sourceMap: isDevMode
-          }
-        }, {
-          loader: 'sass-loader',
-          options: {
-            // NOTE: source maps are needed here for the resolve url loader to work properly
-            sourceMap: true
-          }
-        }]
-      })
-    }, {
-      test: /\.(css|scss)$/,
-      include: /(styles)/,
-      use: extractSass.extract({
-        fallback: 'style-loader',
-        use: [{
-          loader: 'css-loader',
-          options: {
-            importLoaders: 3,
-            sourceMap: isDevMode
-          }
-        }, {
-          loader: 'postcss-loader',
-          options: {
-            sourceMap: isDevMode
-          }
-        }, {
-          loader: 'resolve-url-loader',
-          query: {
-            sourceMap: isDevMode
-          }
-        }, {
-          loader: 'sass-loader',
-          options: {
-            // NOTE: source maps are needed here for the resolve url loader to work properly
-            sourceMap: true
-          }
-        }]
-      })
-    }, {
-      test: /\.(png|jpg|jpeg|gif)$/,
-      use: {
-        loader: 'file-loader?name=/images/[hash].[ext]'
-      }
-    }]
+        },
+      },
+      {
+        test: /\.module\.(css|scss)$/,
+        use: extractSass.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 3,
+                modules: true,
+                sourceMap: isDevMode,
+                localIdentName: isDevMode ? '[name]__[local]___[hash:base64:5]' : '[hash:base64]',
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: isDevMode,
+              },
+            },
+            {
+              loader: 'resolve-url-loader',
+              query: {
+                sourceMap: isDevMode,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                // NOTE: source maps are needed here for the resolve url loader to work properly
+                sourceMap: true,
+              },
+            },
+          ],
+        }),
+      },
+      {
+        test: /\.(css|scss)$/,
+        include: /(styles)/,
+        use: extractSass.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 3,
+                sourceMap: isDevMode,
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: isDevMode,
+              },
+            },
+            {
+              loader: 'resolve-url-loader',
+              query: {
+                sourceMap: isDevMode,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                // NOTE: source maps are needed here for the resolve url loader to work properly
+                sourceMap: true,
+              },
+            },
+          ],
+        }),
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        use: {
+          loader: 'file-loader?name=/images/[hash].[ext]',
+        },
+      },
+    ],
   },
   plugins: [
     extractSass,
     //NOTE: the ordering of these is needed to get the generate html to have the scripts included in the right order
     new webpack.optimize.CommonsChunkPlugin({
-      names: [
-        'application',
-        'libraries-react',
-        'libraries-core',
-        'ua-parser-setup',
-        'ua-parser'
-      ],
-      minChunks: 5
+      names: ['application', 'libraries-react', 'libraries-core'],
+      minChunks: 5,
     }),
     new HtmlWebpackPlugin({
       inject: true,
-      template: __dirname + '/web/index.html',
+      template: `${__dirname}/web/index.html`,
       // NOTE: not quite sure how this works: https://github.com/jantimon/html-webpack-plugin/issues/140#issuecomment-263927464
-      chunksSortMode: function(a, b) {
-        var order = [
-          'ua-parser',
-          'ua-parser-setup',
-          'libraries-core',
-          'libraries-react',
-          'application'
-        ];
+      chunksSortMode(a, b) {
+        let order = ['libraries-core', 'libraries-react', 'application'];
 
         return order.indexOf(a.names[0]) - order.indexOf(b.names[0]);
       },
@@ -159,13 +160,13 @@ var webpackConfig = {
         keepClosingSlash: true,
         minifyJS: true,
         minifyCSS: true,
-        minifyURLs: true
-      }
+        minifyURLs: true,
+      },
     }),
     new CleanObsoleteChunks(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"' + (isDevMode ? 'development' : 'production') + '"'
-    })
+      'process.env.NODE_ENV': `"${isDevMode ? 'development' : 'production'}"`,
+    }),
   ],
   entry: {
     //3rd party libraries
@@ -178,7 +179,7 @@ var webpackConfig = {
       'moment-timezone',
       'popper.js',
       'redux',
-      'store-cacheable'
+      'store-cacheable',
     ],
 
     'libraries-react': [
@@ -188,17 +189,17 @@ var webpackConfig = {
       'react-redux',
       'react-router-redux',
       'react-transition-group',
-      'prop-types'
+      'prop-types',
     ],
 
     //application code
     application: ['./web/app/application.js'],
   },
   output: {
-    path: __dirname + '/web/build',
+    path: `${__dirname}/web/build`,
     publicPath: '/',
-    filename: '[name]-[chunkhash].js'
-  }
+    filename: '[name]-[chunkhash].js',
+  },
 };
 
 if (isDevMode) {
@@ -211,13 +212,13 @@ if (isDevMode) {
   );
   webpackConfig.plugins.push(
     new CircularDependencyPlugin({
-      exclude: /node_modules/
+      exclude: /node_modules/,
     })
   );
 } else {
   webpackConfig.plugins.push(
     new webpack.LoaderOptionsPlugin({
-      minimize: true
+      minimize: true,
     })
   );
 
@@ -225,8 +226,8 @@ if (isDevMode) {
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
       compress: {
-        warnings: false
-      }
+        warnings: false,
+      },
     })
   );
 }
