a65521979b9f4468c702de1de22e8b0a44086c41
Ryan Zec <code@ryanzec.com>
2018-03-02 18:44:03 -0500

    current progress

--- src/components/Form/internal/FormRequiredIcon.tsx
+++ src/components/Form/internal/FormRequiredIcon.tsx
@@ -6,7 +6,11 @@ import * as formCss from 'src/components/Form/internal/formCss';
 
 import SvgIcon from 'src/components/SvgIcon/SvgIcon';
 
-export const FormRequiredIcon = styled(SvgIcon)`
+export interface FormRequiredIconPropTypes {
+  icon: string,
+}
+
+export const FormRequiredIconStyled = styled(SvgIcon)`
   height: ${formCss.variables.sizeRequiredIconInput};
   width: ${formCss.variables.sizeRequiredIconInput};
   color: ${themesCss.light.state.danger};
@@ -14,6 +18,8 @@ export const FormRequiredIcon = styled(SvgIcon)`
   left: 3px;
   top: -5px;
 `;
+
+export const FormRequiredIcon = (props: )
 
 FormRequiredIcon.propTypes = {
   icon: PropTypes.string.isRequired,
