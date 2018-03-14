d7f6bb30788ff02a67b1ca63abb08d88d323ee7c
Ryan Zec <code@ryanzec.com>
2018-03-14 18:25:27 -0400

    now using portals for popover content

--- src/components/Popover/PopoverContent.js
+++ src/components/Popover/PopoverContent.js
@@ -1,13 +1,50 @@
-import styled from 'styled-components';
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+import {createPortal} from 'react-dom';
+import styled, {css} from 'styled-components';
 
 import * as structureCss from 'src/styles/structure';
 import * as themesCss from 'src/styles/themes';
 
-export const PopoverContent = styled.div`
-  padding: 8px;
-  border: 1px solid ${themesCss.light.global.gray4};
-  background-color: ${themesCss.light.global.white};
-  border-radius: ${structureCss.borderRadius.tiny};
+export const containerStyles = (props) => {
+  if (props.isPlain) {
+    return null;
+  }
+
+  return css`
+    padding: 8px;
+    border: 1px solid ${themesCss.light.global.gray4};
+    background-color: ${themesCss.light.global.white};
+    border-radius: ${structureCss.borderRadius.tiny};
+  `;
+};
+
+export const ContainerStyled = styled.div`
+  ${containerStyles};
 `;
+
+ContainerStyled.propTypes = {
+  isPlain: PropTypes.bool,
+};
+
+ContainerStyled.defaultProps = {
+  isPlain: true,
+};
+
+export class PopoverContent extends PureComponent {
+  static propTypes = {
+    isPlain: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    isPlain: true,
+  };
+
+  render() {
+    const node = <ContainerStyled {...this.props} />;
+
+    return createPortal(node, document.querySelector('.react-portals'));
+  }
+}
 
 export default PopoverContent;
